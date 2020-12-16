4dbb38372cfbaf6b1462c14028a2c9fd3c9ae036
m-vahe <vahemartirosyanars@gmail.com>
2020-12-16 17:07:24 +0400

    css structure fixes

--- scenes/shop/index.js
+++ scenes/shop/index.js
@@ -1,9 +1,18 @@
 import ShopHead from "./components/ShopHead";
+import Aside from "./components/Aside";
 
 const ShopScene = () =>{
   return (
       <>
         <ShopHead/>
+        <div className={"d-flex shop-body"}>
+            <div className={"col-lg-3"}>
+                <Aside/>
+            </div>
+            <div className={"d-flex col-lg-9"}>
+
+            </div>
+        </div>
       </>
   )
 }
